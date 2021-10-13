3fffd459947d69c85da47e70f73067c33f1dc3e6
ParkJuneWoo <korca02220@naver.com>
2021-10-13 15:11:44 +0900

    피드가 없을때 화면 추가

---
+++ lib/app/ui/android/home/components/story_card_empty.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StoryCardEmpty extends StatelessWidget {
+  const StoryCardEmpty({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20, right: 10, left: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      width: Get.size.width * 0.85,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              height: Get.size.height * 0.3,
+              child: Image.asset(
+                "assets/images/empty_feed.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              '피드가 없어요... \n이야기를 들려주세요!',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.nanumBrushScript(
+                fontSize: 45,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/ui/android/home/components/story_swipe.dart
+++ lib/app/ui/android/home/components/story_swipe.dart
@@ -1,6 +1,7 @@
 import 'package:ae_stagram_app/app/controller/controllers.dart';
 import 'package:ae_stagram_app/app/controller/home/home_controller.dart';
 import 'package:ae_stagram_app/app/ui/android/home/components/story_card.dart';
+import 'package:ae_stagram_app/app/ui/android/home/components/story_card_empty.dart';
 import 'package:ae_stagram_app/app/ui/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,16 +15,19 @@ class StorySwipe extends GetWidget<HomeController> {
           padding: const EdgeInsets.only(right: 30, left: 30),
           color: mainColor,
           child: Obx(
-            () => ListView.builder(
-                physics: BouncingScrollPhysics(),
-                controller: controller.scrollController,
-                scrollDirection: Axis.horizontal,
-                itemCount: controller.storyCardResult.value.feedInfos.length,
-                itemBuilder: (context, index) {
-                  return StoryCard(
-                    feed: controller.storyCardResult.value.feedInfos[index],
-                  );
-                }),
+            () => controller.storyCardResult.value.feedInfos.length != 0
+                ? ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    controller: controller.scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemCount:
+                        controller.storyCardResult.value.feedInfos.length,
+                    itemBuilder: (context, index) {
+                      return StoryCard(
+                        feed: controller.storyCardResult.value.feedInfos[index],
+                      );
+                    })
+                : StoryCardEmpty(),
           )),
     );
   }
